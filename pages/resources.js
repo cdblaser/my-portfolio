c6b1d975c4e52e6221d80b4a23e4689805d03189
Curtis Blaser <cd13laser@gmail.com>
2022-10-08 19:53:24 -0400

    fixed delay issue on resources page

--- pages/resources.js
+++ pages/resources.js
@@ -18,10 +18,11 @@ const Resources = () => {
             JSON, React, ExpressJS, NextJS
           </p>
         </Section>
-        <Heading as="h3" fontSize={20} mb={4} mt={2}>
-          My collection of the best f2p links to learn web dev
-        </Heading>
+
         <Section delay={0.5}>
+          <Heading as="h3" fontSize={20} mb={4} mt={2}>
+            My collection of the best f2p links to learn web dev
+          </Heading>
           <SimpleGrid columns={[1, 2, 2]} gap={6}>
             <GridItem
               title="Leetcode, SPECIFICALLY Intro to Data Structure courses/cards"
